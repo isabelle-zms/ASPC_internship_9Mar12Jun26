83287009544569046df191f22ad0dc6bb0f9d73b
isabelle-zms <minsi.zai@gmail.com>
2026-06-08 17:54:40 +0800

    added comments and introduced message to JSON conversion

--- sandbox/protobuf-demo/brid-demo/bridpacket_comparison.py
+++ sandbox/protobuf-demo/brid-demo/bridpacket_comparison.py
@@ -1,11 +1,14 @@
 import json
 from google.protobuf.json_format import ParseDict
+from google.protobuf.json_format import MessageToJson
 import bridpacket_pb2
 
-with open('BRID_protobuf/bridpackets.json', 'r') as f:
-   bridpacket_dict = json.load(f)
+with open('bridpackets.json', 'r') as f:
+   bridpacket_dict = json.load(f) # obtain dictionary containing data from the JSON file
 
 # protobuf - 148 bytes
+# Converted dictionary --> protobuf message --> bytes
+# There are alternative methods to convert JSON --> protobuf message, though not demonstrated here
 
 msg_pb = bridpacket_pb2.Packet()    
 ParseDict(bridpacket_dict, msg_pb)
@@ -13,6 +16,11 @@ ParseDict(bridpacket_dict, msg_pb)
 msg_bytes = msg_pb.SerializeToString()
 print("Protobuf:", len(msg_bytes), "bytes")
 print(msg_bytes)
+
+# Converted bytes --> protobuf message --> JSON to verify the integrity of the data after conversion
+msg_pb2 = bridpacket_pb2.Packet()
+msg_pb2.ParseFromString(msg_bytes)
+bridpacket_json = MessageToJson(msg_pb2)
 
 # JSON - 683 bytes
 
